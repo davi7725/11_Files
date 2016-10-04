899ccf19dc0d945c64615abb6c68fe1f2e5ce9b8
davi7725 <davi7725@edu.eal.dk>
2016-10-04 17:01:40 +0200

    Add project files.

---
+++ 11_Files/FileStockRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace _11_Files
+{
+    internal class FileStockRepository : IStockRepository,IFileRepository
+    {
+        private DirectoryInfo repositoryDir;
+
+        List<Stock> listOfStocks = new List<Stock>();
+
+        private long id = 0;
+
+        public FileStockRepository(DirectoryInfo repositoryDir)
+        {
+            this.repositoryDir = repositoryDir;
+        }
+
+        public long NextId()
+        {
+            id++;
+            return id;
+        }
+        public void SaveStock(Stock stockObj)
+        {
+            stockObj.Id = (int)NextId();
+            listOfStocks.Add(stockObj);
+        }
+        public Stock LoadStock(long idNumber)
+        {
+            Stock finalObj = new Stock();
+            foreach(Stock stockObj in listOfStocks)
+            {
+                if(stockObj.Id == idNumber)
+                {
+                    finalObj = stockObj;
+                }
+            }
+            return finalObj;
+        }
+
+        public List<Stock> FindAllStocks()
+        {
+            return new List<Stock>();
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public string StockFileName(int number)
+        {
+            return "stock"+number+".txt";
+        }
+
+        public string StockFileName(Stock number)
+        {
+            return "stock"+number.Id+".txt";
+        }
+
+    }
+}
---
+++ 11_Files/StockIO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _11_Files
+{
+    internal class StockIO
+    {
+        internal void WriteStock(StringWriter sw, Stock obj)
+        {
+
+            sw.WriteLine(obj.Symbol);
+            sw.WriteLine(obj.PricePerShare);
+            sw.WriteLine(obj.NumShares);
+        }
+
+        internal void WriteStock(FileInfo file, Stock obj)
+        {
+
+        }
+
+        internal Stock ReadStock(StringReader data)
+        {
+            Stock finalObj = new Stock(data.ReadLine(), Convert.ToDouble(data.ReadLine()), Convert.ToInt32(data.ReadLine()));
+
+            return finalObj;
+        }
+
+        internal Stock ReadStock(FileInfo file)
+        {
+            return new Stock();
+        }
+    }
+}
